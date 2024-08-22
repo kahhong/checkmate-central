369294be6e4f41fb60f0cec3a057025a6498abc2
kean <kean05.23@ichat.sp.edu.sg>
2024-08-22 16:02:54 +0800

    Initial Commit

---
+++ src/main/java/com/ila/tetrisshowdown/TetrisshowdownApplication.java
@@ -0,0 +1,13 @@
+package com.ila.tetrisshowdown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TetrisshowdownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TetrisshowdownApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ila/tetrisshowdown/TetrisshowdownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ila.tetrisshowdown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TetrisshowdownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

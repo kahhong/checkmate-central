391baca5b98cfd28528a96e05ac89694ba3976f3
Jin <52993042+JLJZ@users.noreply.github.com>
2024-09-13 11:57:17 +0800

    Format code

--- src/main/java/com/ila/checkmatecentral/exceptions/InvalidPasswordException.java
+++ src/main/java/com/ila/checkmatecentral/exceptions/InvalidPasswordException.java
@@ -2,10 +2,14 @@ package com.ila.checkmatecentral.exceptions;
 
 import org.springframework.security.core.AuthenticationException;
 
+/**
+ * Class for exceptions related to passwords that do not conform to security requirements.
+ * @author Jin
+ */
 public class InvalidPasswordException extends AuthenticationException {
 
-	public InvalidPasswordException(String msg) {
-		super(msg);
+	public InvalidPasswordException(String message) {
+		super(message);
 	}
 
 }

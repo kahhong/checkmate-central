dce4071c1f9e9729b506ec81908ec965e4d55493
Codyjtx <jeowcody@gmail.com>
2024-09-13 14:10:08 +0800

    Remove HTTP typing in exceptions
    Allow more flexibility in assigning HTTP response codes when returning exceptions

--- src/main/java/com/ila/checkmatecentral/exceptions/TournamentNotFoundException.java
+++ src/main/java/com/ila/checkmatecentral/exceptions/TournamentNotFoundException.java
@@ -1,9 +1,5 @@
 package com.ila.checkmatecentral.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.NOT_FOUND) // 404 Error
 public class TournamentNotFoundException extends RuntimeException{
     
     TournamentNotFoundException(Long tournamentId) {

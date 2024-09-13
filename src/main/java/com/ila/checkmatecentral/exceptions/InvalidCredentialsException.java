833ba7c8208e84091a13bf052d3ec5c92197e6b5
Jin <52993042+JLJZ@users.noreply.github.com>
2024-09-13 11:57:47 +0800

    Add InvalidCredentialsException

---
+++ src/main/java/com/ila/checkmatecentral/exceptions/InvalidCredentialsException.java
@@ -0,0 +1,11 @@
+package com.ila.checkmatecentral.exceptions;
+
+import org.springframework.security.core.AuthenticationException;
+
+public class InvalidCredentialsException extends AuthenticationException {
+
+    public InvalidCredentialsException() {
+        super("Invalid credentials");
+    }
+
+}

05c143812fd61d99e4a236a01f0569c793519fd8
theofficialericng <theofficialericng@gmai.com>
2024-10-13 01:05:41 +0800

    Added under MatchRepository to list match without password being show

--- src/main/java/com/ila/checkmatecentral/repository/MatchRepository.java
+++ src/main/java/com/ila/checkmatecentral/repository/MatchRepository.java
@@ -6,9 +6,33 @@ import com.ila.checkmatecentral.entity.MatchPk;
 import java.util.List;
 import java.util.Optional;
 
+import com.ila.checkmatecentral.entity.Tournament;
+import com.ila.checkmatecentral.entity.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MatchRepository extends JpaRepository<Match, MatchPk> {
-    Optional<Match> findByMatchId(Integer id);
+
+
+    Optional<Match> findByMatchId(int matchId);
+
+
     Optional<List<Match>> findByTournamentId(Integer tournamentId);
+
+    default Optional<Match> findByMatchIdWithoutPassword(int matchId){
+        Match match =  findByMatchId(matchId).orElseThrow();
+        UserAccount player1 = match.getPlayer1();
+        UserAccount player2 = match.getPlayer2();
+
+        Tournament tournament = match.getTournament();
+
+        List<UserAccount> u1 = tournament.getPlayerList();
+        for(UserAccount u : u1){
+            u.eraseCredentials();
+        }
+        player1.eraseCredentials();
+        player2.eraseCredentials();
+        return Optional.of(match);
+    }
 }

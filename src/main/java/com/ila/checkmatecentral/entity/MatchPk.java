4674d794dbf612b38962f26f9d69d3e81e36f1d0
Jin <52993042+JLJZ@users.noreply.github.com>
2024-10-13 02:12:12 +0800

    Add accessors to MatchPK

--- src/main/java/com/ila/checkmatecentral/entity/MatchPk.java
+++ src/main/java/com/ila/checkmatecentral/entity/MatchPk.java
@@ -2,6 +2,11 @@ package com.ila.checkmatecentral.entity;
 
 import java.io.Serializable;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class MatchPk implements Serializable {
     private Integer tournamentId;
     private int matchId;

fe88e3980fb31202fa9f1dd062ddd4ce95cf00dc
Codyjtx <jeowcody@gmail.com>
2024-10-15 20:42:12 +0800

    Updated RestClient with Demo requests
    Fixed minor issue with MatchPK

--- src/main/java/com/ila/checkmatecentral/entity/MatchPk.java
+++ src/main/java/com/ila/checkmatecentral/entity/MatchPk.java
@@ -4,9 +4,11 @@ import java.io.Serializable;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class MatchPk implements Serializable {
     private Integer tournamentId;
     private int matchId;

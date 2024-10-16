06d33a475e1019f912877efabb92163c16eae491
Codyjtx <jeowcody@gmail.com>
2024-10-16 14:06:00 +0800

    Minor changes for mapping and RestClient

--- src/main/java/com/ila/checkmatecentral/controller/MatchController.java
+++ src/main/java/com/ila/checkmatecentral/controller/MatchController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,7 +51,7 @@ public class MatchController {
     }
 
     @CrossOrigin
-    @PostMapping("/{id}/update")
+    @PutMapping("/{id}/update")
     public ResponseEntity<?> updateMatchOutcome(@PathVariable("id") Integer matchId, @RequestBody JsonNode json) {
         double outcome = json.get("outcome").asDouble();
         try {

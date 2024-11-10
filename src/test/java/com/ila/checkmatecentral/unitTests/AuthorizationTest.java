59381de4237e345cecda565d69253fe4356a8f76
Jin <52993042+JLJZ@users.noreply.github.com>
2024-11-11 03:42:33 +0800

    Fix AuthorizationTest

--- src/test/java/com/ila/checkmatecentral/unitTests/AuthorizationTest.java
+++ src/test/java/com/ila/checkmatecentral/unitTests/AuthorizationTest.java
@@ -1,44 +1,50 @@
 package com.ila.checkmatecentral.unitTests;
 
-import com.ila.checkmatecentral.config.SecurityConfig;
-import com.ila.checkmatecentral.controller.TournamentController;
-import com.ila.checkmatecentral.entity.TournamentType;
-import com.ila.checkmatecentral.service.MatchService;
-import com.ila.checkmatecentral.service.TournamentService;
-import org.json.JSONObject;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.web.servlet.MockMvc;
-
-import java.time.LocalDateTime;
-import java.util.Map;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.ila.checkmatecentral.config.SecurityConfig;
+import com.ila.checkmatecentral.controller.TournamentController;
+import com.ila.checkmatecentral.entity.TournamentType;
+import com.ila.checkmatecentral.service.AccountCredentialService;
+import com.ila.checkmatecentral.service.MatchService;
+import com.ila.checkmatecentral.service.TournamentService;
+
 @WebMvcTest(TournamentController.class)
 @Import(SecurityConfig.class)
 public class AuthorizationTest {
+    
+    private static final String END_POINT = "/api/tournaments/";
+
     @Autowired
     private MockMvc mockMvc;
 
-    @Mock
+    @MockBean
     private TournamentService tournamentService;
 
-    @Mock
-    private UserAccountService userAccountService;
+    @MockBean
+    private AccountCredentialService userAccountService;
 
-    @Mock
+    @MockBean
     private MatchService matchService;
 
     @Test
-    public void testCreateTournament_withUser_returnsUnauthorized() throws Exception {
-        mockMvc.perform(post("/api/tournaments/")
+    @WithMockUser(roles = "ADMIN")
+    void testCreateTournament_withAdmin_returnsCreated() throws Exception {
+        mockMvc.perform(post(END_POINT)
                 .contentType("application/json")
                 .content(new JSONObject(
                     Map.of(
@@ -52,6 +58,24 @@ public class AuthorizationTest {
                 ).toString()))
             .andExpect(status().isCreated())
             .andExpect(content().string("Tournament Created Successfully"));
+    }
+    
+    @Test
+    @WithMockUser(roles = "USER")
+    void testCreateTournament_withUser_returnsUnauthorized() throws Exception {
+        mockMvc.perform(post(END_POINT)
+                .contentType("application/json")
+                .content(new JSONObject(
+                    Map.of(
+                        "name", "Test Tournament",
+                        "description", "Test Tourney",
+                        "maxPlayers", 4,
+                        "minElo", 1000,
+                        "type", TournamentType.SINGLE_KNOCKOUT.name(),
+                        "startDate", LocalDateTime.now().plusDays(1),
+                        "endDate", LocalDateTime.now().plusDays(2))
+                ).toString()))
+            .andExpect(status().isForbidden());
     }
 
 }

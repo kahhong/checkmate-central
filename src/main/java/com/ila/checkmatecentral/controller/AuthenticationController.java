c4b16f716083f298811342d983990adf33ab82da
Sim Kah Hong <skhpotato@proton.me>
2024-11-10 15:08:44 +0800

    Remove unused code.

--- src/main/java/com/ila/checkmatecentral/controller/AuthenticationController.java
+++ src/main/java/com/ila/checkmatecentral/controller/AuthenticationController.java
@@ -12,8 +12,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import com.ila.checkmatecentral.entity.UserAccount;

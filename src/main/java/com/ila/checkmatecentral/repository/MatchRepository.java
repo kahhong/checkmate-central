a5abc04a39b5ebb883c31092a6cda5be423de59b
Jin <52993042+JLJZ@users.noreply.github.com>
2024-11-18 03:52:40 +0800

    Refactor findByMatchIdWithoutPassword()
    Return empty optional instead of throwing default error

--- src/main/java/com/ila/checkmatecentral/repository/MatchRepository.java
+++ src/main/java/com/ila/checkmatecentral/repository/MatchRepository.java
@@ -4,6 +4,8 @@ import com.ila.checkmatecentral.entity.Match;
 import com.ila.checkmatecentral.entity.MatchPk;
 import com.ila.checkmatecentral.entity.Player;
 import com.ila.checkmatecentral.entity.Tournament;
+import com.ila.checkmatecentral.exceptions.MatchNotFoundException;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -17,19 +19,25 @@ public interface MatchRepository extends JpaRepository<Match, MatchPk> {
 
     Optional<List<Match>> findByTournamentId(Integer tournamentId);
 
-    default Optional<Match> findByMatchIdWithoutPassword(int matchId){
-        Match match =  findByMatchId(matchId).orElseThrow();
-        Player player1 = match.getPlayer1();
-        Player player2 = match.getPlayer2();
+    default Optional<Match> findByMatchIdWithoutPassword(int matchId) {
+        return findByMatchId(matchId).map(match -> {
+            if (match == null) {
+                return null;
+            }
 
-        Tournament tournament = match.getTournament();
+            Player player1 = match.getPlayer1();
+            Player player2 = match.getPlayer2();
 
-        List<Player> u1 = tournament.getPlayerList();
-        for(Player u : u1){
-            u.eraseCredentials();
-        }
-        player1.eraseCredentials();
-        player2.eraseCredentials();
-        return Optional.of(match);
+            Tournament tournament = match.getTournament();
+
+            List<Player> u1 = tournament.getPlayerList();
+            for(Player u : u1){
+                u.eraseCredentials();
+            }
+            player1.eraseCredentials();
+            player2.eraseCredentials();
+
+            return match;
+        });
     }
 }

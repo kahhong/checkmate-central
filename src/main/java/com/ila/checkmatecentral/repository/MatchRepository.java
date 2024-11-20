b797228961d9c9116f00bd3795bb12f436a6cada
Jin <52993042+JLJZ@users.noreply.github.com>
2024-11-20 23:10:18 +0800

    Remove unused imports

--- src/main/java/com/ila/checkmatecentral/repository/MatchRepository.java
+++ src/main/java/com/ila/checkmatecentral/repository/MatchRepository.java
@@ -1,15 +1,14 @@
 package com.ila.checkmatecentral.repository;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.ila.checkmatecentral.entity.Match;
 import com.ila.checkmatecentral.entity.MatchPk;
 import com.ila.checkmatecentral.entity.Player;
 import com.ila.checkmatecentral.entity.Tournament;
-import com.ila.checkmatecentral.exceptions.MatchNotFoundException;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-import java.util.Optional;
 
 public interface MatchRepository extends JpaRepository<Match, MatchPk> {
 
